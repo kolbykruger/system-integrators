0a3bf8962af31e4087865033314a36aeb13040af
Kolby Kruger <kolbykruger@gmail.com>
2022-04-22 16:32:02 -0400

    start of navigation design

--- js/fonts.js
+++ js/fonts.js
@@ -1,18 +1,19 @@
-(function() {
+;(function () {
     WebFontConfig = {
-      active: function() {
-        sessionStorage.fonts = true;
-      },
-      google: {
-        families: ['Open+Sans:300,400,400i,500,600,700']
-      },
-      timeout: 2000
-    };
+        active: function () {
+            sessionStorage.fonts = true
+        },
+        typekit: {
+            id: 'raj4epy',
+        },
+        timeout: 2000,
+    }
 
-    (function(d) {
-      var wf = d.createElement('script'), s = d.scripts[0];
-      wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
-      wf.async = true;
-      s.parentNode.insertBefore(wf, s);
-    })(document);
-})();
+    ;(function (d) {
+        var wf = d.createElement('script'),
+            s = d.scripts[0]
+        wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js'
+        wf.async = true
+        s.parentNode.insertBefore(wf, s)
+    })(document)
+})()
--- js/nav.js
+++ js/nav.js
@@ -1,31 +1,84 @@
-//Nav
-//$( "ul.top .trigger" ).each(function(){$( this ).children('a:first').attr( "onclick", "return false" );});
+const menu = document.querySelector('.menu')
+const menuPanelsNodeList = document.querySelectorAll('.menu .menu-panel')
+const menuPanels = Array.from(menuPanelsNodeList)
+const navTriggersNodeList = document.querySelectorAll('.nav .trigger a')
+const navTriggers = Array.from(navTriggersNodeList)
 
-$('.nav .drop_menu').each(function() {
-   if ($(this).find(".item").length >= 8) {
-       $(this).addClass('column_nav');
-   }
-});
+// Menu panels
+const menuPanelLinksNodeList = document.querySelectorAll('.menu .menu-panel a')
+const menuPanelLinks = Array.from(menuPanelLinksNodeList)
 
-var hoverTimeout;
-$('.nav .trigger').hover(function() {
-    clearTimeout(hoverTimeout);
-    $(this).addClass('open').find('.drop').attr('aria-hidden', 'true').attr('aria-expanded', 'false');
-    $('.trigger').not(this).removeClass("open");
-}, function() {
-    var $self=$(this);
-    hoverTimeout = setTimeout(function() {
-        $self.removeClass('open').find('.drop').attr('aria-hidden', 'true').attr('aria-expanded', 'false');
-    }, 250);
-});
+// Menu exit
+const menuExitButton = document.querySelector('.menu .menu-exit')
 
-$('.nav .trigger > a').focusin(function() {
-    var parent = $(this).parent();
-    parent.addClass('open').find('.drop').attr('aria-hidden','false').attr('aria-expanded', 'true');
-    $('.trigger').not(parent).removeClass("open");
-});
+navTriggers.forEach(trigger => {
+    trigger.addEventListener('click', () => {
+        const id = trigger.getAttribute('data-category')
+        console.log(id)
+        // check if it's already open
+        // if it is, close it
+        if (trigger.classList.contains('open')) {
+            closeMenuPanels()
+        } else {
+            openMenuPanels({ trigger, id })
+        }
+    })
+})
 
-$('.nav .drop a:last,.nav .drop input').focusout(function() {
-    var parent = $(this).parents().find('.trigger');
-    parent.removeClass('open').find('.drop').attr('aria-hidden','true').attr('aria-expanded', 'false');
-});
+function closeMenuPanels() {
+    navTriggers.forEach(trigger => {
+        trigger.classList.remove('open')
+    })
+    menuPanels.forEach(panel => {
+        panel.classList.remove('open')
+    })
+    menu.classList.remove('open')
+    document.body.classList.remove('menu-open')
+}
+function openMenuPanels(props) {
+    // remove open from all triggers
+    navTriggers.forEach(trigger => {
+        trigger.classList.remove('open')
+    })
+    // remove open from all menu panels
+    menuPanels.forEach(panel => {
+        panel.classList.remove('open')
+    })
+
+    // add props to appropriate elements
+    props.trigger.classList.add('open')
+    // menuPanels[props.id] ? menuPanels[props.id].classList.add('open') : null
+    const activePanel = menuPanels.filter(panel => {
+        console.log(panel.dataset.category, 'vs', props.id)
+        return panel.dataset.category == props.id
+    })
+    console.log(activePanel)
+    activePanel[0].classList.add('open')
+    menu.classList.add('open')
+    document.body.classList.add('menu-open')
+}
+
+// Menu panel
+menuPanelLinks.forEach(link => {
+    const id = link.getAttribute('data-category')
+    if (id) {
+        link.addEventListener('mouseenter', () => {
+            const menuPanelImagesNodeList = link.closest('.links').nextElementSibling.querySelectorAll('li')
+            const menuPanelImages = Array.from(menuPanelImagesNodeList)
+
+            if (menuPanelImages) {
+                menuPanelImages.forEach(image => {
+                    image.classList.remove('active')
+                })
+
+                const relatedImage = menuPanelImages[id]
+                relatedImage.classList.add('active')
+            }
+        })
+    }
+})
+
+// Menu exit
+// menuExitButton.addEventListener('click', () => {
+//     closeMenuPanels()
+// })
